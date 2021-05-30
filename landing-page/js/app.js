dafe53fccb46797a7fedacfda9b7ba801b5a0518
Silas Jimmy <jimmysilas17@gmail.com>
2021-05-30 09:36:34 +0300

    add: Create new HTML and CSS files

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,10 +30,11 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+
 
 
 // Add class 'active' to section when near top of viewport
@@ -45,13 +46,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
